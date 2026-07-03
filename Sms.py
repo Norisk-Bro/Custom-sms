aa0ca946e639935d5ae159eb2e26b5bacb5b4c8c
༒⃝⃝༆Sᴀᴊɪʙ❤️⃟⃟⃟⃟⃟℠⃟⃟༒ <sajibmeta1day@gmail.com>
2026-07-04 01:30:51 +0600

    Update Sms.py

--- Sms.py
+++ Sms.py
@@ -1,7 +1,1 @@
 import Freesms # এটি আপনার .so ফাইলটিকে লোড করবে
-
-# এবার আপনার মেইন ফাংশনটি কল করুন যা দিয়ে টুলটি চালু হবে
-try:
-    Freesms.send_sms()
-except AttributeError:
-    print("Error: Could not find send_sms function in the module.")
