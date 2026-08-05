a462659ca57302ecbde3ab46933418f45b9b489f
༒⃝⃝༆Sᴀᴊɪʙ❤️⃟⃟⃟⃟⃟℠⃟⃟༒ <sajibmeta1day@gmail.com>
2026-08-06 01:01:41 +0600

    Update Send--sms.py

--- Send--sms.py
+++ Send--sms.py
@@ -1,12 +1,1 @@
-import sys
-import importlib.util
-import importlib.machinery
-
-# CPython C-extension loader setup
-so_filename = "Send.cpython-314-aarch64-linux-android.so"
-
-loader = importlib.machinery.ExtensionFileLoader("Send", so_filename)
-spec = importlib.util.spec_from_loader("Send", loader)
-Send = importlib.util.module_from_spec(spec)
-sys.modules["Send"] = Send
-spec.loader.exec_module(Send)
+import Send
