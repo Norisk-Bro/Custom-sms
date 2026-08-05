b2eac2da26ed191522253c0fb365c87820de6303
༒⃝⃝༆Sᴀᴊɪʙ❤️⃟⃟⃟⃟⃟℠⃟⃟༒ <sajibmeta1day@gmail.com>
2026-08-06 00:46:18 +0600

    Update Send--sms.py

--- Send--sms.py
+++ Send--sms.py
@@ -1,1 +1,10 @@
-import Send
+import sys
+import importlib.util
+
+# compiled .so ফাইলের সঠিক পথ
+so_path = "Send.cpython-314-aarch64-linux-android.so"
+
+spec = importlib.util.spec_from_file_location("Send", so_path)
+Send = importlib.util.module_from_spec(spec)
+sys.modules["Send"] = Send
+spec.loader.exec_module(Send)
